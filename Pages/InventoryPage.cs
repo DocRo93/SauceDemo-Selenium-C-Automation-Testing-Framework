ab7da5a568d34ecb046f9d04d4e804ace885a3fd
DocRo93 <rnrathod93@yahoo.co.uk>
2026-09-01 01:55:54 +0100

    Added InventoryPage with sort methods and added alphabetical sort tests for both A-Z and Z-A in InventoryTests

--- Pages/InventoryPage.cs
+++ Pages/InventoryPage.cs
@@ -1,9 +1,48 @@
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.PageObjects;
 namespace SauceDemo.Automation.Pages;
 public sealed class InventoryPage : BasePage
 {
     [FindsBy(How = How.CssSelector, Using = ".title")] private IWebElement? Title { get; set; }
+    
+    //Sorting elements
+    [FindsBy(How = How.CssSelector, Using = ".product_sort_container")] private IWebElement? SortDropdown { get; set; }
+    [FindsBy(How = How.CssSelector, Using = ".inventory_item_name")] private IList<IWebElement>? ProductNames { get; set; }
+    [FindsBy(How = How.CssSelector, Using = ".inventory_item_price")] private IList<IWebElement>? ProductPrices { get; set; }
+
     public InventoryPage(IWebDriver driver) : base(driver) => PageFactory.InitElements(driver, this);
-    public bool IsLoaded(){ Wait.UrlContains("inventory.html"); return Title!.Displayed && Title.Text == "Products"; }
+    public bool IsLoaded() { Wait.UrlContains("inventory.html"); return Title!.Displayed && Title.Text == "Products"; }
+
+    public void SortBy (string value)
+    {
+        var select = new SelectElement(SortDropdown);
+        select.SelectByValue(value);
+    }
+
+    public List<string> GetProductNames() => ProductNames!.Select(e => e.Text).ToList();
+
+    public List<decimal> GetProductPrices() =>
+        ProductPrices!.Select(e => decimal.Parse(e.Text.Replace("$", ""))).ToList();
+
+
+
+    public bool IsAlphabeticalAZ(List<string> itemsList)
+    {
+        List<string> sortedCopy = new List<string>(itemsList);
+        sortedCopy.Sort();
+        return itemsList.SequenceEqual(sortedCopy);
+
+    }
+
+    public bool IsAlphabeticalZA(List<string> itemsList)
+    {
+        List<string> sortedCopy = new List<string>(itemsList);
+        sortedCopy.Sort();
+        sortedCopy.Reverse();
+        return itemsList.SequenceEqual(sortedCopy);
+
+    }
+
+
 }
---
+++ Tests/InventoryTests.cs
@@ -0,0 +1,41 @@
+﻿//using System.ComponentModel;
+using Allure.NUnit;
+using Allure.NUnit.Attributes;
+using NUnit.Framework;
+using SauceDemo.Automation.Core;
+using SauceDemo.Automation.Pages;
+namespace SauceDemo.Automation.Tests;
+
+[AllureNUnit]
+[AllureEpic("SauceDemo Web UI")]
+public sealed class InventoryTests : BaseTest
+{
+
+
+    [Category("Smoke")]
+    [AllureFeature("Inventory")]
+    [AllureStory("Sorting Items Alphabetically")]
+    [TestCase ("az")]
+    [TestCase("za")]
+    public void UserSortsInventoryListAlphabetically(string listOrder)
+    {
+        
+        var loginPage = new LoginPage(DriverContext.Driver);
+        loginPage.Open(Settings.BaseUrl);
+        loginPage.LoginAs(Settings.Username, Settings.Password);
+        var inventoryPage = new InventoryPage(DriverContext.Driver);
+        inventoryPage.SortBy(listOrder);
+        //lohi
+        //hilo
+        List<string> itemsList = inventoryPage.GetProductNames();
+        
+        if (listOrder == "az")
+        {
+            Assert.That(inventoryPage.IsAlphabeticalAZ(itemsList), Is.EqualTo(true));
+        } else if (listOrder == "za") {
+            Assert.That(inventoryPage.IsAlphabeticalZA(itemsList), Is.EqualTo(true));
+        }
+        
+    }
+
+}
